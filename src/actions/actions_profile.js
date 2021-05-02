d441875bff6dc99a4b14d2ad77d0ac96af3c4a85
Sergio Silva <sergiobtos@hotmail.com>
2021-05-02 19:59:52 -0400

    Added Env Variable

--- src/actions/actions_profile.js
+++ src/actions/actions_profile.js
@@ -1,8 +1,12 @@
 export const PROFILE_FETCHED = 'PROFILE_FETCHED';
 export const PROFILE_EDITED = 'PROFILE_EDITED';
 
+
 export function fetchProfile(){
-    var token = "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";
+    let { REACT_APP_GITHUB_TOKEN } = process.env;
+    
+    var token = REACT_APP_GITHUB_TOKEN;
+    console.log('dentro action: '+ token);
     let header = new Headers({"Content-Type":"application/json", "Authorization":"token "+token});
     return(dispatch) => {
         return fetch('https://api.github.com/users/sergiobtos', {
